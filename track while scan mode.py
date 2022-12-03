83a4a8ebc4eaa7896fa906e0d3011783a8f5daee
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-03 10:54:22 +0500

    Create track while scan mode.py

---
+++ track while scan mode.py
@@ -0,0 +1,18 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+squares = []
+for x in range(10):
+    squares.append(x**2)
+    squares
+
+    
+[0]
+[0, 1]
+[0, 1, 4]
+[0, 1, 4, 9]
+[0, 1, 4, 9, 16]
+[0, 1, 4, 9, 16, 25]
+[0, 1, 4, 9, 16, 25, 36]
+[0, 1, 4, 9, 16, 25, 36, 49]
+[0, 1, 4, 9, 16, 25, 36, 49, 64]
+[0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
